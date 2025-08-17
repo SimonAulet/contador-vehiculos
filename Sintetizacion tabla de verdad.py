7388f5b8b48ceb466ae3ca585ee42f5329ae5309
Simon Aulet <simon.aulet@gmail.com>
2025-08-17 19:07:23 -0300

    expr SOP a partir de tabla de verdad

--- Sintetizacion tabla de verdad.py
+++ Sintetizacion tabla de verdad.py
@@ -14,7 +14,22 @@
 # ---
 
 # %% [markdown]
-# | SC | B2 | B1 | SR | S | B2 | B1 |
+# # Logica secuencuial a partir de la tabla de verdad
+
+# %% [markdown]
+# A partir de la tabla de verdad que pongo a continuación uso `sympy.symbols` para sacar las expresiones booleanas de las 4 salidas
+
+# %% [markdown]
+# - SC: Señal de contador (lleno 1, caso contrario 0)
+# - b1: Botón de accionado para ENTRADA de vehículos
+# - b2: Botón de accionado para SALIDA de vehículos
+# - B1: Barra mecánica de ENTRADA
+# - B2: Barra mecánica de SALIDA
+# - SR: Señal de suma o resta del contador (S(Suma) = 1, S(Resta) = 0)
+# - S: Señal si se hace una acción en el contador o no (Se hace acción 1, CC 0)
+#
+#
+# | SC | b2 | b1 | SR | S | B2 | B1 |
 # |----|----|----|----|---|----|----|
 # | IN | IN | IN |OUT |OUT|OUT | OUT|
 # | 0  | 0  | 0  | X  | 0 | 0  | 0  |
@@ -29,57 +44,43 @@
 # %%
 from sympy.logic import SOPform
 from sympy import symbols
+from sympy import init_printing
+init_printing()
 
 # %%
-SC, B2, B1 = symbols('SC B2 B1')
+SC, b2, b1 = symbols('SC b2 b1') #Entradas
+
 minterms_SR = [[0, 0, 1]]
-dontcare_SR = [[0, 0, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 1]]
+dontcare_SR = [[0, 0, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 1]]#Especifico el dontcare, sympy lo soporta
 minterms_S  = [[0, 0, 1], [0, 1, 0], [1, 1, 0]]
 minterms_B2 = [[0, 1, 0], [0, 1, 1], [1, 1, 0], [1, 1, 1]]
 minterms_B1 = [[0, 0, 1], [0, 1, 1], [1, 1, 1]]
-SR = SOPform([SC, B2, B1], minterms_SR, dontcare_SR)
-S  = SOPform([SC, B2, B1], minterms_S)
-B2 = SOPform([SC, B2, B1], minterms_B2)
-B1 = SOPform([SC, B2, B1], minterms_B1)
+
+#Salidas en forma SOP
+SR = SOPform([SC, b2, b1], minterms_SR, dontcare_SR)
+S  = SOPform([SC, b2, b1], minterms_S)
+B2 = SOPform([SC, b2, b1], minterms_B2)
+B1 = SOPform([SC, b2, b1], minterms_B1)
 
 # %%
-
-A, B = symbols('A B')
-minterms_Y1 = [[0, 1], [1, 0]]  # Filas donde Y1=1
-minterms_Y2 = [[0, 0], [1, 0]]  # Filas donde Y2=1
-
-# Obtener expresiones SOP
-Y1 = SOPform([A, B], minterms_Y1)
-Y2 = SOPform([A, B], minterms_Y2)
-
-print("Y1 =", Y1)  # Y1 = (A & ~B) | (~A & B)
-print("Y2 =", Y2)  # Y2 = ~B
+SR
 
 # %%
-from sympy.logic import SOPform
-from sympy import symbols
-
-# Variables:
-A, B, C = symbols('A B C')
-
-# Minterminos (donde la salida es 1):
-minterms = [[0, 0, 1], [1, 0, 0]]  # Ej: Y=1 en A=0,B=0,C=1 y A=1,B=0,C=0
-
-# Don't cares (donde la salida puede ser 0 o 1):
-dontcares = [[1, 1, 0], [0, 1, 1]]  # Ej: Y es indiferente en estas combinaciones
-
-# Minimización considerando don't cares:
-expr_minimizada = SOPform([A, B, C], minterms, dontcares)
-print(expr_minimizada)
+S
 
 # %%
-from sympy.logic import SOPform
-from sympy import symbols
+B2
 
 # %%
-A, B = symbols('A B')
-minterms_Y1 = [[0, 1], [1, 0]]
-minterms_Y2 = [[0, 0], [1, 0]]
+B1
 
-# %%
-SOPform([A, B], minterms_Y1)
+# %% [markdown]
+# Bien, entonces queda:
+#
+# - $\displaystyle SR = \overline{b_2}$
+#
+# - $\displaystyle S = \left(b_2 \cdot \overline{b_1}\right) + \left(b_1 \cdot \overline{SC} \cdot \overline{b_2}\right)$
+#
+# - $\displaystyle B_2 = b_2$
+#
+# - $\displaystyle B_1 = \left( b_1 \cdot b_2 \right) + \left(b_1 \cdot \overline{SC}\right)$
